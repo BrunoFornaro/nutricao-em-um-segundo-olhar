369a155412fddbda8b10fb0493ed367207fbef62
brunofornaro <brunopereirafornaro@gmail.com>
2023-06-11 00:09:09 -0300

    Altera a tooltip, gráfico de macronutrientes e pratos de hoje
    Altera o estilo da tooltip, função de fechamento da tooltip, estilo dos botões do header, adiciona a logo e conecta o firebase para o gráfico de macronutrientes e os pratos na página "hoje".

--- conect_firebase.js
+++ conect_firebase.js
@@ -32,6 +32,8 @@ console.log(user_uid);
 const confirmar_elemente = document.getElementById('confirmar');
 confirmar_elemente.addEventListener('click', confirmar);
 
+// Ids dos campos em objetivos
+var fieldIds = ["weight", "daily_calories", "carbs_percent", "lipids_percent", "protein_percent", "tolerance", "num_daily_plates"];
 
 // Recupera os dados do usuário
 const docRef = doc(db, "user_info", user_uid);
@@ -40,7 +42,6 @@ getDoc(docRef)
         // Recupera os dados do documento (testes)
         if (docSnap.exists()) {
             if (docSnap.data()["initial"] == false) {
-                let fieldIds = ["weight", "daily_calories", "carbs_percent", "lipids_percent", "protein_percent", "tolerance"];
                 fieldIds.forEach(function (fieldId) {
                     // Define o valor do elemento como o valor no banco de dados
                     document.getElementById(fieldId).value = docSnap.data()[fieldId];
@@ -57,7 +58,6 @@ getDoc(docRef)
 
 
 function confirmar() {
-    var fieldIds = ["weight", "daily_calories", "carbs_percent", "lipids_percent", "protein_percent", "tolerance"];
     var data = {};
 
     fieldIds.forEach(function (fieldId) {
@@ -78,6 +78,7 @@ function confirmar() {
         lipids_percent: data["lipids_percent"],
         protein_percent: data["protein_percent"],
         tolerance: data["tolerance"],
+        num_daily_plates: data["num_daily_plates"],
         initial: 0
     })
         .then(() => {
@@ -89,46 +90,18 @@ function confirmar() {
         });
 
     // Cria (ou atualiza) o documento para o usuário no Firestore (histórico)
-    setDoc(doc(db, user_uid, new Date().toISOString().slice(0, 10), "user_info", "info"), {
+    setDoc(doc(db, user_uid, curr_date_formated(), "user_info", "info"), {
         weight: data["weight"],
         daily_calories: data["daily_calories"],
         carbs_percent: data["carbs_percent"],
         lipids_percent: data["lipids_percent"],
         protein_percent: data["protein_percent"],
         tolerance: data["tolerance"],
+        num_daily_plates: data["num_daily_plates"],
         initial: 0
     })
         .then(() => {
             console.log("Documento atualizado com sucesso!");
-            // Operação de gravação concluída com sucesso
-            // Troca para a página app.html
-            // window.location.href = "app.html";
-
-
-            // Muda para a página app.html
-            // window.location.href = "app.html";
-
-            /*
-            // Recupera os dados do usuário
-            const docRef = doc(db, "user", user_uid);
-            
-            getDoc(docRef)
-                .then((docSnap) => {
-                    
-                    // Recupera os dados do documento (testes)
-                    if (docSnap.exists()) {
-                        console.log("Document data:", docSnap.data());
-                    } else {
-                        console.log("No such document!");
-                    }
-                    
-                    // // Muda para a página app.html
-                    // window.location.href = "app.html";
-                })
-                .catch((error) => {
-                    console.error("Erro ao recuperar os dados:", error);
-                });
-            */
         })
         .catch((error) => {
             // Ocorreu um erro ao gravar no Firestore
@@ -140,74 +113,73 @@ function confirmar() {
 // Botão de confirmar o prato
 const confirmar_prato = document.getElementById('confirmar_prato');
 
-// Adiciona um evento de clique ao botão
+// Adiciona um evento de clique ao botão de confirmar prato
 confirmar_prato.addEventListener('click', async function () {
     // Obtém a quantidade de alimentos no prato
     var qtd_alimentos = document.getElementsByClassName('filtro_textual').length;
-    // console.log(qtd_alimentos);
+    
+    // Função assíncrona para recuperar o número de pratos do dia
+    async function getNumberOfPlates() {
+        var num_pratos = 0;
+        var stop = false;
+      
+        while (!stop) {
+          console.log("Entrou no loop");
+      
+          try {
+            var docRef = doc(
+              db,
+              user_uid,
+              curr_date_formated(),
+              "plates",
+              "" + num_pratos,
+              "food",
+              "1"
+            );
+      
+            // Aguardar a conclusão da chamada assíncrona
+            var docSnap = await getDoc(docRef);
+      
+            if (docSnap.exists()) {
+              num_pratos++;
+              console.log(num_pratos);
+            } else {
+              console.log("Parou o loop com " + num_pratos + " pratos");
+              stop = true;
+            }
+          } catch (error) {
+            console.error("Erro ao recuperar os dados:", error);
+            stop = true;
+          }
+        }
+      
+        return num_pratos;
+      }
+      
+      var num_pratos = 0;
+      // Chama a função assíncrona para obter o número de pratos
+      await getNumberOfPlates().then(function (n) {
+        console.log("Número de pratos: " + n);
+        num_pratos = n;
+      });
+      console.log("Número de pratos: " + num_pratos);
+
     // Itera pelos alimentos
     for (var i = 1; i <= qtd_alimentos; i++) {
         // Obtém o nome do alimento
-        console.log('filtro_textual_' + i);
         var nome_alimento = document.getElementById('filtro_textual_' + i).value;
+        // Obtém a quantidade do alimento
         var quantidade_alimento = document.getElementById('slider_alimento_' + i).value;
+
+        // Verifica se o nome do alimento não é vazio
         if (nome_alimento) {
-            console.log(nome_alimento);
-            // Salva o alimento no banco de dados
-            // Imprime a hora do dia no formato HH:MM:SS
-            var data = new Date();
-            var hora = data.getHours();
-            var minuto = data.getMinutes();
-            var segundo = data.getSeconds();
-            var hora_atual = hora + ":" + minuto + ":" + segundo;
-            // Função assíncrona para recuperar o número de pratos do dia
-            async function getNumberOfPlates() {
-                var num_pratos = 0;
-                var stop = false;
-
-                while (!stop) {
-                    console.log("Entrou no loop");
-
-                    try {
-                        var formated_date = data.toISOString().slice(0, 10);
-                        var docRef = doc(db, user_uid, new Date().toISOString().slice(0, 10), "plates", "" + num_pratos, "food", "1");
-                        var docSnap = await getDoc(docRef);
-
-                        if (docSnap.exists()) {
-                            num_pratos++;
-                            console.log(num_pratos);
-                        } else {
-                            console.log("Parou o loop");
-                            stop = true;
-                        }
-                    } catch (error) {
-                        console.error("Erro ao recuperar os dados:", error);
-                        stop = true;
-                    }
-                }
-
-                console.log("Número de pratos: " + num_pratos);
-                return num_pratos;
-            }
-
-            // Chama a função assíncrona para obter o número de pratos
-            var num_pratos = await getNumberOfPlates();
-
-            // getDoc(docRef)
-            //     .then((docSnap) => {
-            //         // Recupera os dados do documento (testes)
-            //         if (docSnap.exists()) {
-            //             num_pratos = docSnap.data().length;
-            //             console.log(num_pratos);
-            //         }
-            //     })
-            //     .catch((error) => {
-            //         console.error("Erro ao recuperar os dados:", error);
-            //     });
             // Cria (ou atualiza) o documento para o usuário no Firestore (atual)
-            setDoc(doc(db, user_uid, new Date().toISOString().slice(0, 10), "plates", "" + num_pratos, "food", "" + i), {
+            console.log("Vai ser inserido o alimento " + nome_alimento + " com quantidade " + quantidade_alimento + " no prato " + num_pratos)
+            setDoc(doc(db, user_uid, curr_date_formated(), "plates", "" + num_pratos, "food", "" + i), {
                 name: nome_alimento,
-                quantity: quantidade_alimento
+                quantity: quantidade_alimento,
+                // hora no formato hh:mm
+                hour: curr_hour_formated()
             })
                 .then(() => {
                     console.log("Documento atualizado com sucesso!");
@@ -221,3 +193,18 @@ confirmar_prato.addEventListener('click', async function () {
         }
     }
 });
+
+
+function curr_date_formated() {
+    var data = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"});
+    var dia = data.slice(0, 2);
+    var mes = data.slice(3, 5);
+    var ano = data.slice(6, 10);
+    return ano + "-" + mes + "-" + dia;
+
+} 
+
+function curr_hour_formated() {
+    var data = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"});
+    return data.slice(12, 17);
+}
